f8ff877688f3c1de3359b8ff8577324dc3836e9d
Damien Varron <damien.varron@effervea.com>
2014-01-09 21:06:55 +0100

    configure database connection

---
+++ app/Config/database.php
@@ -0,0 +1,27 @@
+<?php
+class DATABASE_CONFIG {
+
+	public $default = array(
+		'datasource' => 'Database/Mysql',
+		'persistent' => false,
+		'host' => 'localhost',
+		'login' => 'root',
+		'password' => '',
+		'database' => 'digisquare',
+		'encoding' => 'utf8'
+	);
+
+	public function __construct() {
+		if (isset($_SERVER) && isset($_SERVER['DB_HOST'])) {
+			$this->default  = array(
+				'datasource' => 'Database/Mysql',
+				'persistent' => false,
+				'host' => $_SERVER['DB_HOST'],
+				'login' => $_SERVER['DB_USER'],
+				'password' => $_SERVER['DB_PASS'],
+				'database' => $_SERVER['DB_NAME'],
+				'encoding' => 'utf8'
+			);
+		}
+	}
+}
