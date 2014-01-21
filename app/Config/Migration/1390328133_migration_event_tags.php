f60d1baae621ec0729f59e31dadc8eb1b6144b9c
juniors74335 <james.rudelin@gmail.com>
2014-01-21 20:35:29 +0100

    Tout qui marche bien !!

--- app/Config/Migration/1390328133_migration_event_tags.php
+++ app/Config/Migration/1390328133_migration_event_tags.php
@@ -55,7 +55,7 @@ class MigrationEventTags extends CakeMigration
                         
                 )
                 
-                )
+                
                 
         ),
                 
