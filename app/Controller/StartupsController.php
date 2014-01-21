9320281f4c639ca0980d841343dac9c4a5abc97f
edouard chusseau <edouard.chusseau@icloud.com>
2014-01-21 16:57:42 +0100

    Ajout du controller Startups

---
+++ app/Controller/StartupsController.php
@@ -0,0 +1,16 @@
+<?php
+App::uses('AppController', 'Controller');
+/**
+ * Startups Controller
+ *
+ */
+class StartupsController extends AppController {
+
+/**
+ * Scaffold
+ *
+ * @var mixed
+ */
+	public $scaffold;
+
+}
