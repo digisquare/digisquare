b5bcc1bfadb415c4bda825e4d4c328ccce9c50cb
DamdamTouch <damien.audrezet@icloud.com>
2014-01-21 16:40:24 +0100

    Ajout du model Startup

---
+++ app/Model/Startup.php
@@ -0,0 +1,16 @@
+<?php
+App::uses('AppModel', 'Model');
+/**
+ * Startup Model
+ *
+ */
+class Startup extends AppModel {
+
+/**
+ * Display field
+ *
+ * @var string
+ */
+	public $displayField = 'name';
+
+}
---
+++ app/Test/Fixture/StartupFixture.php
@@ -0,0 +1,44 @@
+<?php
+/**
+ * StartupFixture
+ *
+ */
+class StartupFixture extends CakeTestFixture {
+
+/**
+ * Fields
+ *
+ * @var array
+ */
+	public $fields = array(
+		'id' => array('type' => 'integer', 'null' => true, 'default' => null, 'key' => 'primary'),
+		'edition_id' => array('type' => 'integer', 'null' => true, 'default' => null),
+		'name' => array('type' => 'string', 'null' => true, 'default' => null, 'collate' => 'utf8_general_ci', 'charset' => 'utf8'),
+		'description' => array('type' => 'text', 'null' => true, 'default' => null, 'collate' => 'utf8_general_ci', 'charset' => 'utf8'),
+		'contacts' => array('type' => 'text', 'null' => true, 'default' => null, 'collate' => 'utf8_general_ci', 'charset' => 'utf8'),
+		'created' => array('type' => 'datetime', 'null' => true, 'default' => null),
+		'modified' => array('type' => 'datetime', 'null' => true, 'default' => null),
+		'indexes' => array(
+			
+		),
+		'tableParameters' => array('charset' => 'utf8', 'collate' => 'utf8_general_ci', 'engine' => 'InnoDB')
+	);
+
+/**
+ * Records
+ *
+ * @var array
+ */
+	public $records = array(
+		array(
+			'id' => 1,
+			'edition_id' => 1,
+			'name' => 'Lorem ipsum dolor sit amet',
+			'description' => 'Lorem ipsum dolor sit amet, aliquet feugiat. Convallis morbi fringilla gravida, phasellus feugiat dapibus velit nunc, pulvinar eget sollicitudin venenatis cum nullam, vivamus ut a sed, mollitia lectus. Nulla vestibulum massa neque ut et, id hendrerit sit, feugiat in taciti enim proin nibh, tempor dignissim, rhoncus duis vestibulum nunc mattis convallis.',
+			'contacts' => 'Lorem ipsum dolor sit amet, aliquet feugiat. Convallis morbi fringilla gravida, phasellus feugiat dapibus velit nunc, pulvinar eget sollicitudin venenatis cum nullam, vivamus ut a sed, mollitia lectus. Nulla vestibulum massa neque ut et, id hendrerit sit, feugiat in taciti enim proin nibh, tempor dignissim, rhoncus duis vestibulum nunc mattis convallis.',
+			'created' => '2014-01-21 16:38:32',
+			'modified' => '2014-01-21 16:38:32'
+		),
+	);
+
+}
