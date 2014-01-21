71e24c808e93e3265e125f8c42ad8737890141ee
Dame17 <d.mali@hotmail.fr>
2014-01-21 21:16:35 +0100

    ajout controller

---
+++ app/Test/Case/Controller/EventsControllerTest.php
@@ -0,0 +1,19 @@
+<?php
+App::uses('EventsController', 'Controller');
+
+/**
+ * EventsController Test Case
+ *
+ */
+class EventsControllerTest extends ControllerTestCase {
+
+/**
+ * Fixtures
+ *
+ * @var array
+ */
+	public $fixtures = array(
+		'app.event'
+	);
+
+}
