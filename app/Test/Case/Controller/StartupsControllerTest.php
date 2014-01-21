92e842a2b6d773069ee12d7527f7b29342bab9c4
chudou <edouard.chusseau@icloud.com>
2014-01-21 22:44:49 +0100

    Ajout test controller startup

---
+++ app/Test/Case/Controller/StartupsControllerTest.php
@@ -0,0 +1,19 @@
+<?php
+App::uses('StartupsController', 'Controller');
+
+/**
+ * StartupsController Test Case
+ *
+ */
+class StartupsControllerTest extends ControllerTestCase {
+
+/**
+ * Fixtures
+ *
+ * @var array
+ */
+	public $fixtures = array(
+		'app.startup'
+	);
+
+}
