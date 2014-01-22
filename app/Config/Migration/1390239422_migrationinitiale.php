6cc93c13ffd6cafd315bdd6f7073b4974cf5c08e
KGiret <kevin.giret@y-nov.com>
2014-01-22 17:30:47 +0100

    Correction Table name User -> Users

--- app/Config/Migration/1390239422_migrationinitiale.php
+++ app/Config/Migration/1390239422_migrationinitiale.php
@@ -18,7 +18,7 @@ class MigrationInitiale extends CakeMigration {
 	public $migration = array(
 		'up' => array(
 			'create_table' => array(
-				'user'=> array(
+				'users'=> array(
 					'id' => array(type => 'integer', 'null' => false, 'default' => NULL, 'length' => 11, 'key' => 'primary'),
 					'username' => array(type => 'string', 'null' => false, 'default' => NULL, 'length' => 255),
 					'password' => array(type => 'string', 'null' => false, 'default' => NULL, 'length' => 255),
