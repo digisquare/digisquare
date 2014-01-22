f9efc39bdbee821312be416bb7d33351f5f46227
Damien Varron <damien.varron@effervea.com>
2014-01-22 12:59:40 +0100

    rename, indent, clean create_startups_tags_table migration

---
+++ app/Config/Migration/1390328487_create_startups_tags_table.php
@@ -0,0 +1,73 @@
+<?php
+class CreateStartupsTagsTable extends CakeMigration {
+
+/**
+ * Migration description
+ *
+ * @var string
+ * @access public
+ */
+	public $description = 'Creates the StartupsTags table';
+
+/**
+ * Actions to be performed
+ *
+ * @var array $migration
+ * @access public
+ */
+	public $migration = array(
+		'up' => array(
+			'create_table' => array(
+				'startup_tags' => array(
+					'id' => array(
+						'type'    => 'integer',
+						'null'    => false,
+						'length'  => 11,
+						'key'     => 'primary'
+					),
+					'startup_id' => array(
+						'type'    => 'integer',
+						'null'    => false,
+						'length'  => 11,
+						'key'     => 'primary'
+					),
+					'tag_id' => array(
+						'type'    => 'integer',
+						'null'    => false,
+						'length'  => 11,
+						'key'     => 'primary'
+					),
+					'indexes' => array(
+						'PRIMARY' => array(
+							'column' => 'id',
+							'unique' => 1
+						),
+					),
+				),
+			),
+		),
+		'down' => array(),
+	);
+
+/**
+ * Before migration callback
+ *
+ * @param string $direction, up or down direction of migration process
+ * @return boolean Should process continue
+ * @access public
+ */
+	public function before($direction) {
+		return true;
+	}
+
+/**
+ * After migration callback
+ *
+ * @param string $direction, up or down direction of migration process
+ * @return boolean Should process continue
+ * @access public
+ */
+	public function after($direction) {
+		return true;
+	}
+}
