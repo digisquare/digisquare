505bfa767556e4a58e143b7623cab6b399e5f607
Damien Varron <damien.varron@effervea.com>
2014-01-22 14:26:53 +0100

    minor indent update on StartupsController

--- app/Controller/StartupsController.php
+++ app/Controller/StartupsController.php
@@ -100,4 +100,5 @@ class StartupsController extends AppController {
 			$this->Session->setFlash(__('The startup could not be deleted. Please, try again.'));
 		}
 		return $this->redirect(array('action' => 'index'));
-	}}
+	}
+}
