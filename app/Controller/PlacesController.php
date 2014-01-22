69a9e37230e5f95246174f977a4ebe96735bdd0a
Damien Varron <damien.varron@effervea.com>
2014-01-22 12:24:03 +0100

    update minor indent typo on PlacesController

--- app/Controller/PlacesController.php
+++ app/Controller/PlacesController.php
@@ -100,4 +100,5 @@ class PlacesController extends AppController {
 			$this->Session->setFlash(__('The place could not be deleted. Please, try again.'));
 		}
 		return $this->redirect(array('action' => 'index'));
-	}}
+	}
+}
