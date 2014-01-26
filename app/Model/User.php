c0cebb17d70ed5b58969e69cdfc1930807cc8ea2
Damien Varron <damien.varron@effervea.com>
2014-01-26 14:42:44 +0100

    implement simple auth

--- app/Model/User.php
+++ app/Model/User.php
@@ -1,46 +1,35 @@
 <?php
 App::uses('AppModel', 'Model');
-/**
- * User Model
- *
- */
+App::uses('SimplePasswordHasher', 'Controller/Component/Auth');
+
 class User extends AppModel {
 
-/**
- * Validation rules
- *
- * @var array
- */
 	public $validate = array(
 		'username' => array(
 			'notEmpty' => array(
 				'rule' => array('notEmpty'),
-				//'message' => 'Your custom message here',
-				//'allowEmpty' => false,
-				//'required' => false,
-				//'last' => false, // Stop validation after this rule
-				//'on' => 'create', // Limit validation to 'create' or 'update' operations
 			),
 		),
 		'password' => array(
 			'notEmpty' => array(
 				'rule' => array('notEmpty'),
-				//'message' => 'Your custom message here',
-				//'allowEmpty' => false,
-				//'required' => false,
-				//'last' => false, // Stop validation after this rule
-				//'on' => 'create', // Limit validation to 'create' or 'update' operations
 			),
 		),
 		'email' => array(
 			'email' => array(
 				'rule' => array('email'),
-				//'message' => 'Your custom message here',
-				//'allowEmpty' => false,
-				//'required' => false,
-				//'last' => false, // Stop validation after this rule
-				//'on' => 'create', // Limit validation to 'create' or 'update' operations
 			),
 		),
 	);
+
+	public function beforeSave($options = array()) {
+		if (isset($this->data['User']['password'])) {
+			$passwordHasher = new SimplePasswordHasher();
+			$this->data['User']['password'] = $passwordHasher->hash(
+				$this->data['User']['password']
+			);
+		}
+		return true;
+	}
+
 }
