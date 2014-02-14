ef1def247ca0f7831e5dc11e133563fd34a536a5
AliceGarden <joanny.saillet@y-nov.com>
2014-02-14 13:48:53 +0100

    mise en place du bouton qui lie la fonction crée presentevent

---
+++ app/Controller/OrganizationsController.php
@@ -0,0 +1,93 @@
+<?php
+App::uses('AppController', 'Controller');
+
+class OrganizationsController extends AppController {
+
+	public function index() {
+		$this->Organization->recursive = 0;
+		$this->set('organizations', $this->Paginator->paginate());
+	}
+
+	public function view($id = null) {
+		if (!$this->Organization->exists($id)) {
+			throw new NotFoundException(__('Invalid organization'));
+		}
+		$options = array('conditions' => array('Organization.' . $this->Organization->primaryKey => $id));
+		$this->set('organization', $this->Organization->find('first', $options));
+	}
+
+	public function add() {
+		if ($this->request->is('post')) {
+			$this->Organization->create();
+			if ($this->Organization->save($this->request->data)) {
+				$this->Session->setFlash(__('The organization has been saved.'));
+				return $this->redirect(array('action' => 'index'));
+			} else {
+				$this->Session->setFlash(__('The organization could not be saved. Please, try again.'));
+			}
+		}
+		$places = $this->Organization->Place->find('list');
+		$editions = $this->Organization->Edition->find('list');
+		$this->set(compact('places', 'editions'));
+	}
+
+	public function edit($id = null) {
+		if (!$this->Organization->exists($id)) {
+			throw new NotFoundException(__('Invalid organization'));
+		}
+		if ($this->request->is(array('post', 'put'))) {
+			if ($this->Organization->save($this->request->data)) {
+				$this->Session->setFlash(__('The organization has been saved.'));
+				return $this->redirect(array('action' => 'index'));
+			} else {
+				$this->Session->setFlash(__('The organization could not be saved. Please, try again.'));
+			}
+		} else {
+			$options = array('conditions' => array('Organization.' . $this->Organization->primaryKey => $id));
+			$this->request->data = $this->Organization->find('first', $options);
+		}
+		$places = $this->Organization->Place->find('list');
+		$editions = $this->Organization->Edition->find('list');
+		$this->set(compact('places', 'editions'));
+	}
+
+	public function delete($id = null) {
+		$this->Organization->id = $id;
+		if (!$this->Organization->exists()) {
+			throw new NotFoundException(__('Invalid organization'));
+		}
+		$this->request->onlyAllow('post', 'delete');
+		if ($this->Organization->delete()) {
+			$this->Session->setFlash(__('The organization has been deleted.'));
+		} else {
+			$this->Session->setFlash(__('The organization could not be deleted. Please, try again.'));
+		}
+		return $this->redirect(array('action' => 'index'));
+	}
+	
+	public function presentevents($id = null){		
+		if (!$this->Organization->exists($id)) {
+			throw new NotFoundException(__('Invalid organization'));
+		}
+		$events_id = $this->Organization->Organizer->find('list', array(
+			'containt' => array(
+				'Event' => array(
+					'Edition','Place'
+				)
+			),
+			'fields' => array(
+				'Organizer.Event_id'
+			),
+			'condition' => array(
+				'Organizer.organization_id' => $id,
+				'Event.end_at >= NOW()'
+			)
+		));
+		$this->loadModel('Event');
+		$events = $this->Paginator->paginate('Event',array(
+			'Event.id' => $events_id
+		));
+		$this->set(compact('events'));
+	}
+
+}
--- app/Model/AppModel.php
+++ app/Model/AppModel.php
@@ -21,4 +21,6 @@ App::uses('Model', 'Model');
  * @package       app.Model
  */
 class AppModel extends Model {
+	public $actsAs = array('Containable'
+	);
 }
