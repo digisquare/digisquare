69f10b45667d2c76b047ab6f26336a56f92e64eb
Damien Varron <damien@yaal.fr>
2015-01-07 20:23:02 +0100

    cleanup AuthenticationsController

--- Controller/AuthenticationsController.php
+++ Controller/AuthenticationsController.php
@@ -10,23 +10,23 @@ class AuthenticationsController extends AppController {
 
 	public function callback() {
 		if (isset($this->data['error'])) {
-			$this->Session->setFlash(__('The user could not be authenticated. Please, try again.') . $this->data['error']);
+			$this->Session->setFlash(__('The user could not be authenticated. Please, try again.') . $this->data['error'], 'message_error');
 			return $this->redirect($this->Auth->loginAction);
 		}
 
 		if ($this->Auth->user()) {
 			$this->Authentication->addCredentials($this->Auth->user('id'), $this->data['auth']);
-			$this->Session->setFlash(__('The authentication has been saved.'));
+			$this->Session->setFlash(__('The authentication has been saved.'), 'message_success');
 			return $this->redirect($this->Auth->redirect());
 		}
 
 		$user = $this->Authentication->updateOrCreateUser($this->data['auth']);
 
 		if ($this->Auth->login($user['User'])) {
-			$this->Session->setFlash(__('You are logged in.'));
+			$this->Session->setFlash(__('You are logged in.'), 'message_success');
 			return $this->redirect($this->Auth->redirect());
 		} else {
-			$this->Session->setFlash(__('The user could not be authenticated. Please, try again.'));
+			$this->Session->setFlash(__('The user could not be authenticated. Please, try again.'), 'message_error');
 			return $this->redirect($this->Auth->loginAction);
 		}
 	}
--- View/Elements/message_success.ctp
+++ View/Elements/message_success.ctp
@@ -1,6 +1,6 @@
 <div class="alert alert-success">
     <button type="button" class="close" data-dismiss="alert">×</button>
-	<strong>Success ! </strong>
+	<strong>Success!</strong>
 	<?php echo h($message); ?>
 </div>
 
