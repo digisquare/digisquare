0b60fcac901c7ae7cd9adb92fd73d66a4507d6fc
Damien Varron <damien@yaal.fr>
2015-01-17 22:27:19 +0100

    update PagesController

--- Controller/PagesController.php
+++ Controller/PagesController.php
@@ -1,41 +1,16 @@
 <?php
-/**
- * Static content controller.
- *
- * This file will render views from views/pages/
- *
- * @link          http://cakephp.org CakePHP(tm) Project
- * @package       app.Controller
- * @since         CakePHP(tm) v 0.2.9
- */
 
 App::uses('AppController', 'Controller');
 
-/**
- * Static content controller
- *
- * Override this controller by placing a copy in controllers directory of an application
- *
- * @package       app.Controller
- * @link http://book.cakephp.org/2.0/en/controllers/pages-controller.html
- */
 class PagesController extends AppController {
 
-/**
- * This controller does not use a model
- *
- * @var array
- */
-	public $uses = array();
+	public $uses = [];
 
-/**
- * Displays a view
- *
- * @param mixed What page to display
- * @return void
- * @throws NotFoundException When the view file could not be found
- *	or MissingViewException in debug mode.
- */
+	public function beforeFilter() {
+		parent::beforeFilter();
+		$this->Auth->allow(['display']);
+	}
+
 	public function display() {
 		$path = func_get_args();
 
