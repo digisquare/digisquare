b677fffd3bd4a17a69aef033060240ffe6e2914a
Damien Varron <damien@yaal.fr>
2015-01-25 00:09:20 +0100

    make slugs unique

--- Model/Slug.php
+++ Model/Slug.php
@@ -12,18 +12,26 @@ class Slug extends AppModel {
 	);
 
 	public function createFromVenue($venue) {
-		// TODO: make slugs unique
+		$slug_name = $this->slugifyVenue($venue);
+		$slug = $this->find('first', [
+			'contain' => false,
+			'conditions' => [
+				'Slug.name' => $slug_name
+			]
+		]);
+		if ($slug) {
+			return $slug['Slug']['id'];
+		}
 		$this->create();
-		$slug = $this->slugifyVenue($venue);
 		$this->set('venue_id', $venue['Venue']['id']);
-		$this->set('name', $slug);
+		$this->set('name', $slug_name);
 		$this->save();
 		return $this->id;
 	}
 
 	public function slugifyVenue($venue) {
-		$slug = $this->Venue->implode($venue);
-		$slug = preg_replace('`[0-9]*`', '', $slug);
-		return strtolower(Inflector::slug($slug, '-'));
+		$slug_name = $this->Venue->implode($venue);
+		$slug_name = preg_replace('`[0-9]*`', '', $slug_name);
+		return strtolower(Inflector::slug($slug_name, '-'));
 	}
 }
